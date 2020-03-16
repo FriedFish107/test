323a04d6381ea159c7507ea8d427ad637632d752
FriedFish <a123@Vc-2.local>
2020-03-16 09:02:35 +0800

    python

---
+++ test1/__init__.py
@@ -0,0 +1,16 @@
+a = 10
+while a > 6:
+
+    a = a - 1
+    print(a)
+
+#a , b = eval(input("input two interger nubmer:"))
+#print(a,b,sep='0')
+isum = 0
+for i in range(0,100,18):
+    print(i)
+    isum += i
+print(isum)
+print(sum(range(101)))
+print(isum,end = '')
+print('t')
---
+++ test1/turtletest.py
@@ -0,0 +1,6 @@
+import turtle as tu
+
+
+tu.screensize(640, 480, bg = 'blue')
+tu.getpen
+tu.goto(100,100)
